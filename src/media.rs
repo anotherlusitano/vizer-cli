079dd09c6551abc294887706c357b3dad851290a
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2024-03-02 21:23:38 +0000

    Add poster_url field to Media struct

--- src/media.rs
+++ src/media.rs
@@ -4,10 +4,15 @@ use core::fmt;
 pub struct Media {
     pub title: String,
     pub url: String,
+    pub poster_url: String,
 }
 
 impl fmt::Display for Media {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Title - {} ; Link - {}", self.title, self.url)
+        write!(
+            f,
+            "Title - {} ; Link - {} ; Poster Url - {}",
+            self.title, self.url, self.poster_url
+        )
     }
 }
